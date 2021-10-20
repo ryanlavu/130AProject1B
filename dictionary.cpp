bbc152afe4cbc7165edc25c65ce986ab1d45d6e3
Matthew Tran <matthewgtran@umail.ucsb.edu>
2021-10-20 12:35:55 -0700

    basic outline

---
+++ dictionary.cpp
@@ -0,0 +1,21 @@
+using namespace std;
+
+Dictionary(String fname, String paramFileName) {
+
+	// Temporary Prints
+	// Include hash24 dump function here
+	cout << "Number of words = " << words << endl;
+	cout << "Table size = " << tableSize << endl;
+	cout << "Max collisions" << maxCol << endl;
+	// For loop for # of primary slots with number of words
+	cout << "** Words in the slot with most collisions ***" << endl;
+	// For loop to print all words in the slot with most collisions
+	// For loop for # of secondary hash tables trying hash functions
+	cout << "Average # of hash functions tried = " << avgTries << endl;
+	cout << "Queries:" << endl;
+	// Query print depending on find function
+}
+
+bool find(String word) {
+	return false;
+}
