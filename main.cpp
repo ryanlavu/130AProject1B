9a6c4ec4fb0bc109d5efee61578654a6a654c678
Matthew Tran <matthewgtran@umail.ucsb.edu>
2021-10-23 16:44:35 -0700

    added includes in main

--- main.cpp
+++ main.cpp
@@ -3,15 +3,13 @@
 #include <string>
 #include <vector>
 #include <cmath>
+#include "hash24.cpp"
+#include "dictionary.cpp"
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
 
-
-
-
-
-	
+		
 
 }
