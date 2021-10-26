0e050d8a0519f85fc94c844a8b9d6d06c79ad273
Matthew Tran <matthewgtran@umail.ucsb.edu>
2021-10-25 18:16:38 -0700

    maybe?

--- dictionary.cpp
+++ dictionary.cpp
@@ -266,7 +266,7 @@ Dictionary::Dictionary(string fname, string paramFileName) {
 		
 		if(hashTable[hashValue] != nullptr) {
 			
-			int hash2Value = hashArray[hashValue]->hash(word) % hashTable[hashValue]->size();
+			int hash2Value = hashArray[hashValue]->hash(word) % hashTable[hashValue]->size() - 1;
 
 			if(hashTable[hashValue][hash2Value] == word) {
 
